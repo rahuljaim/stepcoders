bee22a6340ebf0316efca65b6201513d28c37d5e
Rahul Jain <rahuljait@gmail.com>
2022-02-10 18:24:28 +0530

    Updates

---
+++ precache-manifest.19b4db0b1c225af5dc1c3a8977ca1de7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68bae204313b209e1b75a86ec58f32d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcf06dfe59ca0e7518b0",
+    "url": "/static/css/main.742eb482.chunk.css"
+  },
+  {
+    "revision": "fb12950b91a2b9249302",
+    "url": "/static/js/2.13dee516.chunk.js"
+  },
+  {
+    "revision": "a05418ccfc692fc466fc",
+    "url": "/static/js/3.1142b564.chunk.js"
+  },
+  {
+    "revision": "fcf06dfe59ca0e7518b0",
+    "url": "/static/js/main.c11c3909.chunk.js"
+  },
+  {
+    "revision": "7a4970977b4644f95016",
+    "url": "/static/js/runtime~main.52f372aa.js"
+  },
+  {
+    "revision": "7004c4fc8fc118579127c698b4cf44c5",
+    "url": "/static/media/[Step]Coder_banner1.7004c4fc.jpg"
+  },
+  {
+    "revision": "746d7697d9da90bcc59bcd8a7fb0d9f6",
+    "url": "/static/media/findwork.746d7697.jpg"
+  },
+  {
+    "revision": "ca23ba922fe229ba363b1331014fe175",
+    "url": "/static/media/hiring.ca23ba92.jpg"
+  },
+  {
+    "revision": "17b1de028464cc10f761ac71bdc6470b",
+    "url": "/static/media/knowmore.17b1de02.jpg"
+  }
+]);
